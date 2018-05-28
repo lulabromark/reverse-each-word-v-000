4e31dd2923aa0510d068250cbf45893812f6294c
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-28 20:17:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
-  sentence = sentence.join(" ")
+  sentence = sentence.split(" ")
 words = sentence.collect{|word| word.reverse}
-words.split(" ")
+words.join(" ")
 
 end
